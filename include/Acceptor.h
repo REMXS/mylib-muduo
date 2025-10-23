59a30ffec7070148fd1a0777f58b0e549d958f1a
RMX@linux <zuodui13@gmail.com>
2025-10-23 17:28:07 +0800

    完成了acceptor模块的编写

---
+++ include/Acceptor.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include<functional>
+
+#include<Channel.h>
+#include"Socket.h"
+#include"noncopyable.h"
+
+class InetAddress;
+class EventLoop;
+
+class Acceptor :noncopyable
+{
+public:
+    using NewConnectionCallBack=std::function<void(int sockfd,const InetAddress&)>;
+private:
+    EventLoop* loop_;//listenfd 所在的loop
+
+    //socket和channel中绑定的fd就是listenfd，fd的声明周期和socket绑定，socket析构时，fd关闭
+    Socket accept_socket_; //专门用于接听新连接的socket
+    Channel accept_channel_;//专门用于接听新连接的channel
+
+    bool listening_;
+
+    NewConnectionCallBack connection_callback_;//用于处理新建立连接的函数
+
+
+    void handRead();//listenfd 的读回调函数
+
+public:
+    Acceptor(EventLoop*loop,const InetAddress&addr,bool reuse);
+    ~Acceptor();
+
+    void listen(); //设置listenfd的listen状态
+    bool isListening(){return listening_;}
+
+    void setConnetionCallback(NewConnectionCallBack cb){connection_callback_=std::move(cb);}
+};
+
+
---
+++ src/Acceptor.cc
@@ -0,0 +1,75 @@
+#include<arpa/inet.h>
+#include<errno.h>
+
+#include"Acceptor.h"
+#include"Logger.h"
+#include"Timestamp.h"
+#include"InetAddress.h"
+
+
+//创建一个非阻塞的fd
+static int createNonBlocking()
+{
+    int fd=socket(AF_INET,SOCK_STREAM|SOCK_NONBLOCK|SOCK_CLOEXEC,IPPROTO_TCP);
+    if(fd<0)
+    {
+        LOG_FATAL("%s:%s:%d listen socket create error",__FILE__,__FUNCTION__,__LINE__)
+    }
+    return fd;   
+}
+
+
+Acceptor::Acceptor(EventLoop*loop,const InetAddress&addr,bool reuse)
+    :loop_(loop)
+    ,listening_(false)
+    ,accept_socket_(createNonBlocking())
+    ,accept_channel_(loop,accept_socket_.fd())
+{
+    accept_socket_.setReuseAddr(true);
+    accept_socket_.setReusePort(reuse);
+    accept_socket_.bindAddress(addr);
+    accept_channel_.setReadCallback([this](Timestamp){
+        this->handRead();
+    });
+}
+
+
+Acceptor::~Acceptor()
+{
+    accept_channel_.disableAll();
+    accept_channel_.remove();
+}
+
+void Acceptor::listen()
+{
+    listening_=true;
+    accept_socket_.listen();
+    accept_channel_.enableReading();//要使用enableReading listenfd才能加入poller被监听读事件
+}
+
+
+void Acceptor::handRead()
+{
+    InetAddress addr;
+    int connfd=accept_socket_.accept(addr);
+
+    if(connfd>0)
+    {
+        if(connection_callback_)
+        {
+            connection_callback_(connfd,addr);
+        }
+        else
+        {
+            ::close(connfd);//如果没有对新连接处理的函数，则关闭新的连接
+        }
+    }
+    else
+    {
+        LOG_ERROR("%s:%s:%d accept error:%d",__FILE__,__FUNCTION__,__LINE__,errno);
+        if(errno==EMFILE)
+        {
+            LOG_ERROR("%s:%s:%d socket fd reached limit",__FILE__,__FUNCTION__,__LINE__)
+        }
+    }
+}
